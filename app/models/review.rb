2da76fce0e1c66028172f3f6affffe5f4a62f2c8
Sophia Eady <s.eady@outlook.com>
2020-11-11 12:44:33 +0000

    dhioewnfoin

---
+++ app/models/review.rb
@@ -0,0 +1,6 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+  validates :rating, presence: true
+  validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5] }, numericality: {only_interger: true}
+  validates :content, presence: true
+end
