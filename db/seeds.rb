9626c9ab79b446b3a4bc4da6a89752d2241ec93c
Sophia Eady <s.eady@outlook.com>
2020-11-11 15:19:44 +0000

    ifniofn

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(
+  name: 'Le Canard',
+  address: '1 Hoxton Road',
+  category: 'french'
+)
+
+Restaurant.create(
+  name: 'The Golden Duck',
+  address: '2 Hoxton Road',
+  category: 'chinese'
+)
+
+Restaurant.create(
+  name: "L'Anatra",
+  address: '3 Hoxton Road',
+  category: 'italian'
+)
+
+Restaurant.create(
+  name: 'Ahiru',
+  address: '4 Hoxton Road',
+  category: 'japanese'
+)
+
+Restaurant.create(
+  name: 'De Eend',
+  address: '5 Hoxton Road',
+  category: 'belgian'
+)
